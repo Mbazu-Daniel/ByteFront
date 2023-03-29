41309e80e064bae7f9023733d11f4f130d0b3748
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-03-29 11:31:11 +0100

    security: create middlewares to validate routes

--- server.js
+++ server.js
@@ -5,18 +5,23 @@ const MONGO_URI = process.env.MONGO_URI;
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/connectDB");
+const { notFound, errorHandler } = require("./middlewares/errors");
 const authRouter = require("./routes/users.routes");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Routers
-app.use("/auth", authRouter);
-
 // Health check
 app.use("/health", (req, res) => {
   res.send("Welcome to the Blog");
 });
+
+// Routers
+app.use("/auth", authRouter);
+
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const start = async () => {
   try {
